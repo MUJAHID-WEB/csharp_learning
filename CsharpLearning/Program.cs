b02db66738d1c42f2d190860689e9e4333a59a44
MUJAHID-WEB <mujahid30390@gmail.com>
2024-03-18 10:12:00 +0600

    first commit

---
+++ CsharpLearning/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, C#!");
+
+Random dice = new Random();
+int roll = dice.Next(1,10);
+
+Console.WriteLine(roll);
+
+Console.WriteLine(dice.Next(1,6));
